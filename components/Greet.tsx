9bed1a34a3742692e18b2a5cad76c6a6b9e442af
syofyanzuhad <sofyanzuhad2@gmail.com>
2023-01-29 16:46:10 +0700

    fix: selftClosing-typescript

--- components/Greet.tsx
+++ components/Greet.tsx
@@ -87,7 +87,7 @@ export default function Greet() {
     <div>
       {loading ? (
         <div className="flex justify-center items-center">
-          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12 mb-4"></div>
+          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12 mb-4" />
           <span>Loading...</span>
         </div>
       ) : (
