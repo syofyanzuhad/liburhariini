a05878dfda30e7738dc7f82eeb11b724b8bffe2d
syofyanzuhad <sofyanzuhad2@gmail.com>
2023-01-29 16:44:58 +0700

    fix: fragment-mainLoading

--- components/Greet.tsx
+++ components/Greet.tsx
@@ -84,7 +84,7 @@ export default function Greet() {
   // console.log(isHoliday, 'isHoliday')
 
   return (
-    <>
+    <div>
       {loading ? (
         <div className="flex justify-center items-center">
           <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12 mb-4"></div>
@@ -133,6 +133,6 @@ export default function Greet() {
             </div>
           </div>
       )}
-    </>
+    </div>
   )
 }
